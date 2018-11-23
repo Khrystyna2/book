d9645aca71940c0e727542c079e57ba38186fee5
Khrystyna Khozhai <l.xrustja@gmail.com>
2018-11-23 16:19:23 +0200

    added file

---
+++ js/main-js.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+
+    $('.language>li').on('click', function() {
+      $(this).children('ul').toggleClass('box-shadow').slideToggle();
+      $(this).children('svg').toggleClass('rotate');
+      $('.language').toggleClass('box-shadow')
+    });
+
+    $('.menu-dropdown').on('click', function() {
+      $(this).children('ul').toggleClass('box-shadow').slideToggle();
+      $(this).children('svg').toggleClass('rotate');
+      $('.menu-dropdown').toggleClass('box-shadow')
+    });
+
+    $('.sayings__list>li>ol>li').hover(function() {
+      $(this).toggleClass('open-comment');
+    });
+
+    //fixed section search
+    // let heightSectionSearch = $('.search').outerHeight(true);
+    // $('<div class="clone-search"></div>').insertBefore('.search').css('height', heightSectionSearch).hide();
+    $(window).scroll(function(){
+      let heightHeader = $('.header').height();
+      let heightSectionSearch = $('.search').outerHeight(true);
+      if($(window).scrollTop() >= heightHeader) {
+        $('.search').addClass('sticky');
+        // $('.clone-search').show();
+        $('#sayings').css('padding-top', heightSectionSearch);
+      } else {
+        $('.search').removeClass('sticky');
+        // $('.clone-search').hide();
+        $('#sayings').css('padding-top', "0");
+
+      }
+    });
+  
+});
